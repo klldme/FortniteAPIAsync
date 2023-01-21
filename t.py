163e57f504c5410a6645064d1bf4c9d327132c52
klld <kll.d.repl.co@hotmail.com>
2023-01-21 12:06:25 +0300

    Create t.py

---
+++ t.py
@@ -0,0 +1,28 @@
+import urllib.request
+from os import system
+
+system("pip install py-cord")
+
+from os import environ as ENV
+
+app = Sanic("images")
+
+@app.route("/")
+async def index(request):
+  return redirect("")
+
+@app.route("/Provicali.otf")
+async def Provicali(request):
+  return await file("Provicali.otf")
+
+
+
+
+
+
+
+app.run(
+  host='0.0.0.0', 
+  port=8080 
+  workers=4
+)
